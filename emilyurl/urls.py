26b1b673915d79fe7a0b2f4b1f038fdf36b2bdf4
Emily <efoley326@gmail.com>
2021-11-15 13:32:11 -0500

    import type in urls

--- emilyurl/urls.py
+++ emilyurl/urls.py
@@ -15,7 +15,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from .views import home_view, redirect_url_view
+from urlshortener.views import home_view, redirect_url_view
 
 urlpatterns = [
     path('admin/', admin.site.urls),
