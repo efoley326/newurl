d6aded5348cddd553987490eb98a2ec5f6ac8c09
Emily <efoley326@gmail.com>
2021-11-15 13:30:03 -0500

    home urls

--- emilyurl/urls.py
+++ emilyurl/urls.py
@@ -14,8 +14,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from .views import home_view, redirect_url_view
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', home_view, name='home'),
+    path('<str:shortened_part>', redirect_url_view, name='redirect')
 ]
